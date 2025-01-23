a84e8008c7241a1513987a03e7efa011d17fab46
Matthew Serafica <matthewserafica01@gmail.com>
2025-01-23 22:31:15 +0800

    Refactor MyImages component fixed the header and main content

--- frontend/src/pages/MyImages.vue
+++ frontend/src/pages/MyImages.vue
@@ -1,5 +1,13 @@
 <template>
-    MyImages
+    <header class="bg-white shadow">
+        <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">My Images</h1>
+        </div>
+    </header>
+    <main>
+        <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        </div>
+    </main>
 </template>
 
 <script setup></script>
