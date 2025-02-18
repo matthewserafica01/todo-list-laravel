dc494e8cb12883db22caeba010ee2fed7f60315d
Matthew Serafica <matthewserafica01@gmail.com>
2025-02-19 00:28:47 +0800

    Update Login.vue added functionality to the login form

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -2,12 +2,19 @@
     <GuestLayout>
         <h2 class="mt-10 font-bold tracking-tight text-center text-gray-900 text-2xl/9">Sign in to your account</h2>
 
+        <div class="flex flex-col items-center justify-center flex-1">
+            <div v-if="errorMessage" class="px-3 py-2 mt-4 text-center text-white bg-red-400 rounded lg:w-1/4 sm:w-1/2">
+                {{ errorMessage }}
+            </div>
+        </div>
+
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" action="#" method="POST">
+            <form @submit.prevent="submit" class="space-y-6">
                 <div>
                     <label for="email" class="block font-medium text-gray-900 text-sm/6">Email address</label>
                     <div class="mt-2">
                         <input type="email" name="email" id="email" autocomplete="email" required=""
+                            v-model="data.email"
                             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                 </div>
@@ -21,6 +28,7 @@
                     </div>
                     <div class="mt-2">
                         <input type="password" name="password" id="password" autocomplete="current-password" required=""
+                            v-model="data.password"
                             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                 </div>
@@ -44,6 +52,33 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import GuestLayout from '../components/GuestLayout.vue';
+import axiosClient from '../axios';
+import router from '../router';
 
+const data = ref({
+    email: '',
+    password: '',
+});
+
+const errorMessage = ref('')
+
+function submit() {
+    axiosClient.get('/sanctum/csrf-cookie')
+        .then(response => {
+            axiosClient.post("/login", data.value, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    router.push({ name: 'Home' });
+                })
+                .catch(error => {
+                    console.log(error.response)
+                    errorMessage.value = error.response.data.message;
+                });
+        });
+}
 </script>
