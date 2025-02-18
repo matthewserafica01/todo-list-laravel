17a829458433057082820bac9facf98f21fa37ca
Matthew Serafica <matthewserafica01@gmail.com>
2025-02-19 00:26:37 +0800

    Update main.js Integrate pinia to the project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './style.css';
+
+const pinia = createPinia()
 
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app')
